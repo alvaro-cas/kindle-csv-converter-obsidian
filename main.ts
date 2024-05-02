ace23e834426b22f14fb4403e0e291e115bc3dcb
Álvaro <cuatroy2.com>
2024-05-01 22:19:36 -0600

    Improve code syntax

--- main.ts
+++ main.ts
@@ -6,37 +6,37 @@ import { KindleSettings } from './src/settings';
 import { Reader } from './src/modal_read';
 
 export default class KindleCsvPlugin extends Plugin {
-  async onload() {
-    await this.loadSettings();
+    async onload() {
+	await this.loadSettings();
 
-    this.addRibbonIcon("library", "Kindle CSV Converter", () => {
-      new Reader(this.app, this.settings).open();
-    });
+	this.addRibbonIcon("library", "Kindle CSV Converter", () => {
+	    new Reader(this.app, this.settings).open();
+	});
 
-    this.addCommand({
-      id: 'choose-file',
-      name: 'Choose File',
-      checkCallback: (checking: boolean) => {
-        if (!checking) {
-          new Reader(this.app, this.settings).open();
-        }
-        return true;
-      }
-    })
+	this.addCommand({
+	    id: 'choose-file',
+	    name: 'Choose File',
+	    checkCallback: (checking: boolean) => {
+		if (!checking) {
+		    new Reader(this.app, this.settings).open();
+		}
+		return true;
+	    }
+	})
 
-    this.addSettingTab(new KindleSettings(this.app, this));
+	this.addSettingTab(new KindleSettings(this.app, this));
 
-  }
+    }
 
-  onunload() {
-    console.log('Unloading plugin');
-  }
+    onunload() {
+	console.log('Unloading plugin');
+    }
 
-  async loadSettings() {
-    this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-  }
+    async loadSettings() {
+	this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+    }
 
-  async saveSettings() {
-    await this.saveData(this.settings);
-  }
+    async saveSettings() {
+	await this.saveData(this.settings);
+    }
 }
